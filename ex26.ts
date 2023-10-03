fc73b81e595459bd19d01d8d39c2479df6f65d8d
João Malafaia <j.malafaia02@gmail.com>
2023-10-03 12:20:06 -0300

    commit do exercício de type script

---
+++ ex26.ts
@@ -0,0 +1,11 @@
+function multiplicação(arg1: number, arg2: number): number {
+    return arg1 * arg2
+}
+
+multiplicação(5, 6)
+
+function saudação(nome: string) {
+    return `Olá ${nome}`
+}
+
+saudação('João')
